663eab79ec70339d4f7a3f567d888c8a24124e9b
farhana sherin c <sherinthanseer@gmail.com>
2025-05-20 13:24:32 +0530

    first commit

---
+++ operations.py
@@ -0,0 +1,69 @@
+a=10
+b=15
+print(" addition")
+p=a+b
+print("a+b=",p)
+print("subtraction")
+q=a-b
+print("a-b=",q)
+print("multiplication")
+r=a*b
+print("a*b=",r)
+print(" division")
+s=a/b
+print("a/b=",s)
+print(" exponential")
+t=a**b
+print("a**b=",t)
+print("modulus")
+u=a%b
+print("a%b=",u)
+print("conditional operators")
+if a==b:
+    print("true")
+else:
+    print("false")
+if a!=b:
+    print("true")
+else:
+    print("false")
+if a>b:
+    print("true")
+else:
+    print("false")
+if a<b:
+    print("true")
+else:
+    print("false")
+if a>=b:
+    print("true")
+else:
+    print("false")
+if a<=b:
+    print("true")
+else:
+    print("false")
+print("logical operators")
+x=10
+y=5
+print("and")
+g=(x>y)and(x<y)
+print(g)
+print("or")
+g=(x>y)or(x<y)
+print(g)
+print("not")
+g=not(x>y)and(x<y)
+print(g)
+x+=10
+print(x)
+x/=10
+print(x)
+x-=10
+print(x)
+x*=10
+print(x)
+
+    
+
+
